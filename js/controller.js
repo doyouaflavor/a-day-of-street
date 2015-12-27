8e7d8b686f5a6598259e3e7a3adc0603dcf3c57f
junsuwhy <junsuwhy@gmail.com>
2015-12-27 09:37:36 +0800

    Add options images.

--- js/controller.js
+++ js/controller.js
@@ -1,65 +1,87 @@
 var game = angular.module('game',['ngSanitize']);
-    game.controller('MainCtrl', function () {  
-        this.index = 0;
-        this.score = 0;
-        this.selected = [];
-        this.data = data[this.index];
-        this.last_selected_option = null;
-        this.state='ready';
-        
-        gamePrepare();
-        
-        //按下開始
-        this.start = function(){
+game.controller('MainCtrl', ['$scope', function (scope) {  
+    this.index = 0;
+    this.score = 0;
+    this.selected = [];
+    this.data = data[this.index];
+    this.last_selected_option = null;
+    this.state='ready';
+    scope.optionsStyle = {};
+    scope.answerInfoContentStyle = {};
+
+    gamePrepare();
+
+    //按下開始
+    this.start = function(){
+        this.state = 'question';
+//        $('.game-start').animate({'opacity':0},1500,function(){
+//            $('.game-start').hide();
+//            $('.question').show().animate({'opacity':1},1500);
+//        });
+        setTimeout(function(){
+            scope.optionsStyle = {
+//                height: ($(window).height() - $('.question-content').outerHeight() - 45) + 'px'
+                height: ($(window).height() - 111) + 'px'
+            };
+        },100);
+        console.log(this.state);
+    }
+
+    //按下
+    this.select = function(option){
+        this.state = 'answer-information'; 
+        this.last_selected_option = option;
+        this.selected.push({
+            "information":this.data.information,
+            "option":option,
+        });
+        switch(option.addormulti){
+                    case "+":
+                        this.score+=option.score;
+                    break;
+                    case "*":
+                        this.score*=option.score;
+                    break;
+        }
+        scope.answerInfoContentStyle = {
+            height: ($(window).height() - 121) + 'px'
+        };
+        resizing();
+    }
+
+    // 按下
+    this.next = function(){
+        this.index++;
+        if(this.index >= data.length){
+            this.state = 'ending';
+            endofgame();
+        }else{
             this.state = 'question';
-//            $('.game-start').animate({'opacity':0},1500,function(){
-//                $('.game-start').hide();
-//                $('.question').show().animate({'opacity':1},1500);
-//            });
-            console.log(this.state);
+            this.data = data[this.index];
         }
-
-        //按下
-        this.select = function(option){
-            this.state = 'answer-information'; 
-            this.last_selected_option = option;
-            this.selected.push({
-                "information":this.data.information,
-                "option":option,
-            });
-            switch(option.addormulti){
-                        case "+":
-                            this.score+=option.score;
-                        break;
-                        case "*":
-                            this.score*=option.score;
-                        break;
-            console.log(this.state);
-        }
-
-        // 按下選項
-        this.next = function(){
-            
-            
-            
-            this.index++;
-            if(this.index >= data.length){
-                this.state = 'ending';
-                endofgame();
-            }else{
-                this.state = 'question';
-                this.data = data[this.index];
-                
-                }
-            }
-        }
-    });
-
-function endofgame(){
-    
-};
+        
+        scope.optionsStyle = {
+            height: ($(window).height() - 111) + 'px'
+        };
+        resizing();
+    }
+}]);
 
 function gamePrepare(){
 //    $('.question').hide().css('opacity',0);
 //    $('.final').hide().css('opacity',0);
+}
+
+$(window).onresize(resizing);
+
+function resizing(){
+    var w = $(window).height();
+    if($('.answer-set').length > 0){
+        $('.answer-info-content').height(w - 121);
+    }
+    if($('.options').length > 0){
+        var new_w = w - $('.question-content').outerHeight() - 45;
+        $('.options')
+        $('.options').height(w - 121);
+    }
 }
--- js/data.js
+++ js/data.js
@@ -37,12 +37,14 @@ var data = [
                      "value":"獨立",
                      "addormulti":"+",
                      "score":20,
+                     "img":"img/option-2-1.jpg",
                  },
                  {
                      "name":"B",
                      "value":"集團",
                      "addormulti":"+",
                      "score":15,
+                     "img":"img/option-2-2.jpg",
                  }
              ],
             "information":"許多人對於街賣集團有著許多疑問，人生百味團隊實地走訪了台北兩個團體，發現其多為合作社或一般公司形式進行，有些為出外打拚的身障者提供正餐與住宿，有些因交通安全考量提供接送。台北地區目前有六至七個街賣團體，此外更有大誌、華山地瓜媽媽、人生百味等街頭商品。<br/>獨立與團體街賣各有優劣勢，但街頭並不只屬於行人，也屬於暫時住宿者，以及在街頭上討生活的人們。"
@@ -56,24 +58,28 @@ var data = [
                      "value":"玉蘭花",
                      "addormulti":"+",
                      "score":10,
+                     "img":"img/option-3-1.png",
                  },
                  {
                      "name":"B",
                      "value":"口香糖",
                      "addormulti":"+",
                      "score":5,
+                     "img":"img/option-3-2.png",
                  },
                  {
                      "name":"C",
                      "value":"衛生用品(牙線、衛生紙)",
                      "addormulti":"+",
                      "score":10,
+                     "img":"img/option-3-3.png",
                  },
                  {
                      "name":"D",
                      "value":"彩券",
                      "addormulti":"+",
                      "score":5,
+                     "img":"img/option-3-4.png",
                  },
              ],
             "information":"目前市面上的街頭商品，多以玉蘭花、彩券與日用品為主。<br/>由於無法大量批發，以及每月能販售的數量有限，為了維持生計多會將價格訂得高於市價。但也因此產生被消費者認為太貴且品質不清楚，而不願意購買的困境。"
@@ -85,18 +91,21 @@ var data = [
                      "value":"捷運站出口",
                      "addormulti":"*",
                      "score":3,
+                     "img":"img/option-4-1.jpg",
                  },
                  {
                      "name":"B",
                      "value":"馬路上",
                      "addormulti":"*",
                      "score":1,
+                     "img":"img/option-4-2.jpg",
                  },
                  {
                      "name":"C",
                      "value":"夜市",
                      "addormulti":"*",
                      "score":2,
+                     "img":"img/option-4-3.jpg",
                  }
              ],
             "information":"街賣者多為身體行動較不便者，因此選擇地點時機能與人潮同樣重要。廁所、飲水機回家交通的便利性都是十分重要的考量。<br/>另外有分流動或定點兩種賣法，不一定會每日出現在同點。人氣街賣者就像POP-UP SHOP一樣，撞見時會充滿驚喜呢。"
@@ -109,18 +118,21 @@ var data = [
                      "value":"製作看板",
                      "addormulti":"*",
                      "score":2,
+                     "img":"img/option-5-1.jpg",
                  },
                  {
                      "name":"B",
                      "value":"大聲叫賣",
                      "addormulti":"*",
                      "score":1,
+                     "img":"img/option-5-2.jpg",
                  },
                  {
                      "name":"C",
                      "value":"安靜等客",
                      "addormulti":"*",
                      "score":3,
+                     "img":"img/option-5-3.jpg",
                  }
              ],
             "information":"許多人常認為，街賣是一份低門檻的工作，所以才會有許多弱勢者從事，但低技術門檻的工作比比階是，超過九成過去有其他工作經驗，且街賣也絕非低門檻的工作，夏天早上悶熱、下午暴雨，冬天賣到四肢冰冷，天氣再熱也不敢喝太多水，因為行動不方便，而廁所距離很遠，吃飯要找無障礙空間，被警察取締，還要面對面向路人推銷忍耐冷潮熱諷，若不是有高度的覺悟與毅力，絕對撐不了幾天就放棄了。"
@@ -133,12 +145,14 @@ var data = [
                      "value":"是",
                      "addormulti":"*",
                      "score":2,
+                     "img":"img/option-6-1.jpg",
                  },
                  {
                      "name":"B",
                      "value":"否",
                      "addormulti":"*",
                      "score":1,
+                     "img":"img/option-6-2.jpg",
                  }
              ],
             "information":"以同情的基礎的銷售，就好像是一種捐款，必需不斷的向消費者傳達自身「匱乏」的訊息才能得到生活所需的收入，因此買賣雙方其實很難形成真誠的關係，並且一旦讓消費者明顯的感覺到，你生活改善了，很有可能就造成這樣收入的減少甚至是終止。<br/>另一方面，對於是否需要渲染同情，街賣者心中其實也充滿矛盾。除了怕破壞隱性的同情關係之外，也可能是因擔心弄巧成拙影響收入。這類矛盾與壓抑的心態，自然會阻礙街賣者與消費者做更深的互動。"
